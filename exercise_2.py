d6e168a723b2d06232c21ba2735ec865a492683b
Reid Witte <mwitte2@uncc.edu>
2023-01-17 14:27:59 -0500

    Adding module 2 exercise 2 script

---
+++ exercise_2.py
@@ -0,0 +1,10 @@
+#Reid Witte
+#using w3schools as a reference
+
+stringInput = input("Enter a string: ")
+stringOutput = ""
+
+for x in stringInput:
+    stringOutput = x + stringOutput
+
+print(stringOutput)
